4fbf762e61ff8ec652220ae41d39e4e3d2bc380e
Andrew Williams <1williamsaj@gmail.com>
2019-07-26 11:19:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -12,10 +12,10 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    if cart.keys.include?(coupon[:item]) #Checks to see if our cart includes a couponed item
+    if cart.keys.include?(coupon[:item]) #Checks to see if our cart includes an item that has a coupon
       if cart[coupon[:item]][:count] >= coupon[:num] #If we have a couponed item, it is greater than or equal to the amount needed for the coupon
         itemwithCoupon = "#{coupon[:item]} W/COUPON" #If it meets the requirements, a new hash is created for the item and is assigned to a variable to make life easier
-        if cart[itemwithCoupon]
+        if cart[itemwithCoupon] #If our cart has an item meeting coupon requirements
           cart[itemwithCoupon][:count] += coupon[:num]
           cart[coupon[:item]][:count] -= coupon[:num]
         else
